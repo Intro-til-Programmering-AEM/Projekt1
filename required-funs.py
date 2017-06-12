bb5ea8e98feea4be9e5474cfc0d77096102d8d53
Marcus Medom Ryding <mrok4a@gmail.com>
2017-06-12 17:06:35 +0200

    Added outline for mandatory functions

---
+++ required-funs.py
@@ -0,0 +1,8 @@
+def dataLoad(filename):
+    print("Error: not implemented yet") # TODO
+
+def dataStatistics(data, statistic):
+    print("Error: not implemented yet") # TODO
+
+def dataPlot(data):
+    print("Error: not implemented yet") # TODO
