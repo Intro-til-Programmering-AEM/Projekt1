a0a029083c84e9c6a666e96e0f4a9b11641ed7af
Marcus Medom Ryding <mrok4a@gmail.com>
2017-06-12 16:58:59 +0200

    Added menu outline

---
+++ main.py
@@ -0,0 +1,49 @@
+import sys
+
+def main():
+    print("Welcome to the bacterial data analysis program!")
+    print("Your options:")
+    while(True):
+        option = menu(main_options)
+        if option == 1:
+            print("TODO: not implemented yet")
+        elif option == 2:
+            print("TODO: not implemented yet")
+        elif option == 3:
+            print("TODO: not implemented yet")
+        elif option == 4:
+            print("TODO: not implemented yet")
+        elif option == 5:
+            print("Thank you for using the bacterial data analysis program.")
+            sys.exit()
+        # No else needed, it has already been checked that the option is legal
+
+main_options = {
+    1: "Load data",
+    2: "Filter data",
+    3: "Display statistics",
+    4: "Generate plots",
+    5: "Quit",
+}
+
+def menu(options):
+    # Print options with option numbers
+    for n, t in options.items():
+        print(str(n)+". "+t+".")
+    return input_option(options)
+
+def input_option(options):
+    while(True):
+        try:
+            # Get number that may be a legal option
+            x = int(input("Select an option: "))
+            # Check if it's legal
+            if x in options.keys():
+                return x
+            else:
+                print("Not an option, please try again.")
+        except ValueError:
+            print("Please input a number corresponding to the option you want to select.")
+            pass
+
+main()
