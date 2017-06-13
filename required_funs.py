bbe918736648a6d27998255562301ae556c96511
Marcus Medom Ryding <mrok4a@gmail.com>
2017-06-13 13:58:00 +0200

    Added separate functions for plots

--- required_funs.py
+++ required_funs.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 def dataLoad(filename): #Det antages, at filen findes
     try:    
         data=pd.read_csv(filename,sep=' ')
@@ -10,6 +12,13 @@ def dataStatistics(data, statistic):
     print("Error: not implemented yet") # TODO
 
 def dataPlot(data):
+    plotNumbers(data)
+    plotGrowthRates(data)
+
+def plotNumbers(data):
+    print("Error: not implemented yet") # TODO
+
+def plotGrowthRates(data):
     print("Error: not implemented yet") # TODO
 
 bacteria_types = {
