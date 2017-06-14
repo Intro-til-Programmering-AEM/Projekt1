df098fff766e1334d90799d0751f8579eafdc5ea
Wedenborg <emiliewedenborg@gmail.com>
2017-06-14 14:47:44 +0200

    Fixed #2

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from os import path
 def main():
     print("Welcome to the bacterial data analysis program!")
     filters = []
+    data = None
     while(True):
         print_filters(filters)
         print("Your options:")
@@ -16,61 +17,64 @@ def main():
             if originalData is not None:
                 data = originalData
                 print("Succesfully imported "+str(len(data))+" rows of data.")
-        elif option == 2:
-            option = menu(filter_options)
-            if option is None:
-                pass
-            elif option == 1: # Adding
-                print("Which column would you like to filter on?")
-                option = menu(column_options)
-                print("Which kind of filter would you like to add?")
+        if data is not None:
+            if option == 2:
+                option = menu(filter_options)
                 if option is None:
                     pass
-                elif option == 3:
-                    choice = menu(categorical_options)
-                    print("Which bacteria type?")
-                    bacterium = menu(list(bacteria_types.values()))
+                elif option == 1: # Adding
+                    print("Which column would you like to filter on?")
+                    option = menu(column_options)
+                    print("Which kind of filter would you like to add?")
+                    if option is None:
+                        pass
+                    elif option == 3:
+                        choice = menu(categorical_options)
+                        print("Which bacteria type?")
+                        bacterium = menu(list(bacteria_types.values()))
+                        if choice is None:
+                            pass
+                        elif choice == 1:
+                            filters.append((lambda r: r.Bacteria != bacterium, "Exclude "+bacteria_types[bacterium]))
+                        else:
+                            filters.append((lambda r: r.Bacteria == bacterium, "Include only "+bacteria_types[bacterium]))
+                    else:
+                        choice = menu(continuous_options)
+                        bound = input_float("Please input your bound: ")
+                        def filter_fun(row):
+                            col = row.Temperature if option == 1 else row.GrowthRate
+                            return col > bound if choice == 1 else col < bound
+                        filter_text = column_options[option]+" must be "+("greater than" if choice == 1 else "less than")+" "+str(bound)
+                        filters.append((filter_fun,filter_text))
+                elif option == 2: # Deleting
+                    print("Which filter would you like to remove?")
+                    filter_texts = list(map(lambda f: f[1], filters))
+                    filter_texts.append("All filters")
+                    choice = menu(filter_texts)
                     if choice is None:
                         pass
-                    elif choice == 1:
-                        filters.append((lambda r: r.Bacteria != bacterium, "Exclude "+bacteria_types[bacterium]))
+                    elif choice == len(filter_texts):
+                        filters = []
                     else:
-                        filters.append((lambda r: r.Bacteria == bacterium, "Include only "+bacteria_types[bacterium]))
-                else:
-                    choice = menu(continuous_options)
-                    bound = input_float("Please input your bound: ")
-                    def filter_fun(row):
-                        col = row.Temperature if option == 1 else row.GrowthRate
-                        return col > bound if choice == 1 else col < bound
-                    filter_text = column_options[option]+" must be "+("greater than" if choice == 1 else "less than")+" "+str(bound)
-                    filters.append((filter_fun,filter_text))
-            elif option == 2: # Deleting
-                print("Which filter would you like to remove?")
-                filter_texts = list(map(lambda f: f[1], filters))
-                filter_texts.append("All filters")
-                choice = menu(filter_texts)
-                if choice is None:
+                        del filters[choice-1]
+                # Refilters data every time a filter is added or deleted, inefficient!
+                data = originalData
+                for f in filters:
+                    data = data[f[0](data)]
+                print(str(len(data))+" rows left after filtering")
+            elif option == 3:
+                print("Please choose the kind of statistic you would like to calculate:")
+                option = menu(statistic_options)
+                if option is None:
                     pass
-                elif choice == len(filter_texts):
-                    filters = []
                 else:
-                    del filters[choice-1]
-            # Refilters data every time a filter is added or deleted, inefficient!
-            data = originalData
-            for f in filters:
-                data = data[f[0](data)]
-            print(str(len(data))+" rows left after filtering")
-        elif option == 3:
-            print("Please choose the kind of statistic you would like to calculate:")
-            option = menu(statistic_options)
-            if option is None:
-                pass
-            else:
-                statistic = statistic_options[option]
-                print(dataStatistics(data,statistic))
-        elif option == 4:
-            dataPlot(data)
-        elif option == 5:
+                    statistic = statistic_options[option]
+                    print(dataStatistics(data,statistic))
+            elif option == 4:
+                 dataPlot(data)
+        else:
+            print("Please load valid data before attempting to use this function")
+        if option == 5:
             print("Thank you for using the bacterial data analysis program.")
             sys.exit()
         # No else needed, it has already been checked that the option is legal
