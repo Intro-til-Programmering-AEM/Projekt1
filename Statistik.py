b5fdfb297c4509cbc159f0fe6e837936b3bb935e
Wedenborg <emiliewedenborg@gmail.com>
2017-06-15 12:24:47 +0200

    Emilie rydder op part 2

---
+++ Statistik.py
@@ -0,0 +1,20 @@
+#Denne fil indeholder statistik
+
+#Nedenstående funktion laver de statistik-funktioner, der er krævet i oplægget:
+def dataStatistics(data, statistic):
+    if statistic == "Mean Temperature":
+        return np.mean(data.Temperature)
+    elif statistic == "Mean Growth rate":
+        return np.mean(data.GrowthRate)
+    elif statistic == "Std Temperature":
+        return np.sd(data.Temperature)
+    elif statistic == "Std Growth rate":
+        return np.sd(data.GrowthRate)
+    elif statistic == "Rows":
+        return(len(data))
+    elif statistic == "Mean Cold Growth rate":
+        return np.mean(data.GrowthRate[data.Temperature<20])
+    elif statistic == "Mean Hot Growth rate":
+        return np.mean(data.GrowthRate[data.Temperature>50])
+
+#Nedenstående funktion laver ekstra statistik
--- required_funs.py
+++ required_funs.py
@@ -49,38 +49,5 @@ def dataStatistics(data, statistic):
     elif statistic == "Mean Hot Growth rate":
         return np.mean(data.GrowthRate[data.Temperature>50])
 
-def dataPlot(data,filters=[]):
-    plotWrapper(plotNumbers, data, filters)
-    plotWrapper(plotGrowthRates, data, filters)
-    plotWrapper(boxPlotGrowthRates, data, filters)
-    plotWrapper(boxPlotTemperatures, data, filters)
 
-def plotWrapper(plotFun,data, filters):
-    plotFun(data)
-    if filters != []:
-        plt.title("Filters:"+", ".join(filters))
-    plt.show()
-
-def plotNumbers(data):
-    counts = [list(data.Bacteria).count(i) for i in bacteria_types.keys()]
-    plt.bar(list(bacteria_types.keys()), counts, tick_label = list(bacteria_types.values()))
-    plt.title("Numbers of each type of bacteria")
-    plt.ylabel("Number of bacteria")
-
-def plotGrowthRates(data):
-    points = [[] for i in bacteria_types]
-    for i, r in data.iterrows():
-        points[int(r.Bacteria) - 1].append((r.Temperature, r.GrowthRate))
-    points = [sorted(pointList,key=lambda t: t[0]) for pointList in points] # [[(temp, growthRate)]]
-    xyPairList = [zip(*pointList) for pointList in points] # [[(temps),(growthRates)]]
-    flattened = [item for pair in xyPairList for item in pair] # [(temps),(growthRates),(temps),...]
-    plt.plot(*flattened)
-    label = [bacteria_types[i] for i in set(data.Bacteria)]
-    plt.xlabel("Temperature")
-    plt.ylabel("Growth rate")
-    plt.grid()
-    plt.suptitle("Growth rate by temperature")
-    plt.legend(label,loc="upper right")
-    plt.xlim(10,60)
-    plt.ylim(ymin=0)
 
